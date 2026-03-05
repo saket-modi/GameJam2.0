93ee915ef5ba7632cef995f9fcb3edc8a650cf8c
Saket <saketmodi787@gmail.com>
2026-03-06 00:31:26 +0530

    Added pickups

--- Scripts/pickup.gd
+++ Scripts/pickup.gd
@@ -1,1 +1,24 @@
+@tool
 extends Area2D
+
+enum States {HEART, SCORE, ATTACK}
+
+@export var state : States
+
+func _ready():
+	match state:
+		States.HEART: $AnimatedSprite2D.play("heart")
+		States.SCORE: $AnimatedSprite2D.play("score")
+		States.ATTACK: $AnimatedSprite2D.play("attack")
+
+func _process(_dt):
+	if Engine.is_editor_hint():
+		match state:
+			States.HEART: $AnimatedSprite2D.play("heart")
+			States.SCORE: $AnimatedSprite2D.play("score")
+			States.ATTACK: $AnimatedSprite2D.play("attack")
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name != "Player":
+		return
+	queue_free()
